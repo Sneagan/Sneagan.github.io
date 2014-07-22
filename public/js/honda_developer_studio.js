6c837c9e4684d6eb455088686e36245f8eecb889
Jackson Egan <jackson@sneagan.com>
2014-07-22 11:49:26 -0500

    Architectural fixes.

--- public/js/honda_developer_studio.js
+++ public/js/honda_developer_studio.js
@@ -1,6 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-
-},{}],2:[function(require,module,exports){
 var VisibilityDetector = function(){};
 VisibilityDetector.prototype.fireIfVisible = function(callback) {
   var self = this;
@@ -30,4 +28,6 @@ VisibilityDetector.prototype.isHidden = function(el) {
 };
 
 module.exports = VisibilityDetector;
+},{}],2:[function(require,module,exports){
+
 },{}]},{},[1,2]);
