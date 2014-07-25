8fa91a9f2acf377765aadc22deb0705ac3a54f20
Jackson Egan <jackson@sneagan.com>
2014-07-25 10:17:18 -0500

    To spec circles

--- source/js/constructors/DrawCircle.js
+++ source/js/constructors/DrawCircle.js
@@ -12,48 +12,52 @@ DrawCircle.prototype.init = function($el1, $el2, $el3, $el4) {
 DrawCircle.prototype.animate = function() {
   if (this.dotted) {
     this.dotted.clearRect(0, 0, 400, 400);
-    this.dotted.translate(200/2, 200/2);
-    this.dotted.rotate(Math.PI / 720);
-    this.dotted.translate(-200/2, -200/2);
+    this.dotted.translate(400/2, 400/2);
+    this.dotted.rotate(Math.PI / -720);
+    this.dotted.translate(-400/2, -400/2);
     this.dotted.beginPath();
-    this.dotted.setLineDash([2,10]);
-    this.dotted.arc(100,100,40,0,2*Math.PI);
-    this.dotted.lineWidth = 10;
+    this.dotted.setLineDash([2,6.6]);
+    this.dotted.arc(200,200,115,0,2*Math.PI);
+    this.dotted.lineWidth = 7;
+    this.dotted.strokeStyle = '#ed2435';
     this.dotted.stroke();
   }
   if (this.fat_seg) {
     this.fat_seg.clearRect(0, 0, 400, 400);
-    this.fat_seg.translate(200/2, 200/2);
-    this.fat_seg.rotate(Math.PI / -720);
-    this.fat_seg.translate(-200/2, -200/2);
+    this.fat_seg.translate(400/2, 400/2);
+    this.fat_seg.rotate(Math.PI / -360);
+    this.fat_seg.translate(-400/2, -400/2);
     this.fat_seg.beginPath();
-    this.fat_seg.setLineDash([2,10]);
-    this.fat_seg.arc(100,100,50,0,2*Math.PI);
-    this.fat_seg.lineWidth = 10;
+    //this.fat_seg.setLineDash([2,10]);
+    this.fat_seg.arc(200,200,100,180,2*Math.PI);
+    this.fat_seg.lineWidth = 15;
+    this.fat_seg.strokeStyle = '#ed2435';
     this.fat_seg.stroke();
   }
 
   if (this.thin_long_seg) {
     this.thin_long_seg.clearRect(0, 0, 400, 400);
-    this.thin_long_seg.translate(200/2, 200/2);
+    this.thin_long_seg.translate(400/2, 400/2);
     this.thin_long_seg.rotate(Math.PI / 720);
-    this.thin_long_seg.translate(-200/2, -200/2);
+    this.thin_long_seg.translate(-400/2, -400/2);
     this.thin_long_seg.beginPath();
-    this.thin_long_seg.setLineDash([2,10]);
-    this.thin_long_seg.arc(100,100,60,0,2*Math.PI);
-    this.thin_long_seg.lineWidth = 10;
+    //this.thin_long_seg.setLineDash([2,10]);
+    this.thin_long_seg.arc(200,200,125,360,2*Math.PI);
+    this.thin_long_seg.lineWidth = 5;
+    this.thin_long_seg.strokeStyle = '#ed2435';
     this.thin_long_seg.stroke();
   }
 
   if (this.thin_short_seg) {
     this.thin_short_seg.clearRect(0, 0, 400, 400);
-    this.thin_short_seg.translate(200/2, 200/2);
-    this.thin_short_seg.rotate(Math.PI / -720);
-    this.thin_short_seg.translate(-200/2, -200/2);
+    this.thin_short_seg.translate(400/2, 400/2);
+    this.thin_short_seg.rotate(Math.PI / 360);
+    this.thin_short_seg.translate(-400/2, -400/2);
     this.thin_short_seg.beginPath();
-    this.thin_short_seg.setLineDash([2,10]);
-    this.thin_short_seg.arc(100,100,70,0,2*Math.PI);
-    this.thin_short_seg.lineWidth = 10;
+    //this.thin_short_seg.setLineDash([2,10]);
+    this.thin_short_seg.arc(200,200,100,350,2*Math.PI);
+    this.thin_short_seg.lineWidth = 5;
+    this.thin_short_seg.strokeStyle = '#ed2435';
     this.thin_short_seg.stroke();
   }
 };
